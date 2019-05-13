67de0fd334fe4f726d69999f3789f55bd3ed88bf
Barbara Makowiec <barbara.makowiec@wp.pl>
2019-05-13 18:41:43 +0200

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {Button} from 'reactstrap';
+
+class App extends React.Component {
+
+  render(){
+    return (
+      <div className="App">
+        <Button color="danger">Przyciśk</Button>
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
